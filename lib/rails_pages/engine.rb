14e32c4c38de20046684b0f665765a9d22a1f971
Nigel Baillie <metreckk@gmail.com>
2021-12-03 15:57:40 +0900

    Fix autoloader issues with page.rb

--- lib/rails_pages/engine.rb
+++ lib/rails_pages/engine.rb
@@ -5,5 +5,13 @@ module RailsPages
     config.generators do |g|
       g.test_framework :rspec
     end
+
+    initializer 'rails_pages.zeitwerk' do
+      Rails.application.config.paths['app'].each do |app_path|
+        Rails.autoloaders.each do |loader|
+          loader.ignore File.join(app_path, 'pages')
+        end
+      end
+    end
   end
 end
